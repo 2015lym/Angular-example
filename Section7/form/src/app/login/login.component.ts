c955053e71b80c328284b0ba137f19c63133ba73
2015lym <18622797149@163.com>
2019-01-28 11:57:41 +0800

    更新

--- Section7/form/src/app/login/login.component.ts
+++ Section7/form/src/app/login/login.component.ts
@@ -7,9 +7,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
+  username: string = '';
+  passowrd: string = '';
+  remember: boolean = false;
+
   constructor() { }
 
   ngOnInit() {
   }
 
+  onSubmit() {
+    alert(`账号：${this.username} 密码：${this.passowrd} 记住密码：${this.remember}`);
+  }
 }
