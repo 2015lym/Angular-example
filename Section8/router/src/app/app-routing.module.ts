6510057a4dd6ec0d327da969878faf157dbe92d2
2015lym <18622797149@163.com>
2019-02-11 10:07:12 +0800

    更新

--- Section8/router/src/app/app-routing.module.ts
+++ Section8/router/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AComponent } from './a/a.component';
+import { BComponent } from './b/b.component';
+import { CComponent } from './c/c.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'A', component: AComponent },
+  { path: 'B', component: BComponent },
+  { path: 'C', component: CComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
