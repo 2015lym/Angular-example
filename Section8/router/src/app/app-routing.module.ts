ef26a7f1d3991d347d0ad8390aafbea7c896cd3b
2015lym <18622797149@163.com>
2019-02-12 14:27:55 +0800

    更新

--- Section8/router/src/app/app-routing.module.ts
+++ Section8/router/src/app/app-routing.module.ts
@@ -5,14 +5,14 @@ import { BComponent } from './b/b.component';
 import { CComponent } from './c/c.component';
 
 const routes: Routes = [
-  { path: '', component: AComponent},
+  { path: '', component: AComponent },
   { path: 'A', component: AComponent },
   { path: 'B', component: BComponent },
   { path: 'C', component: CComponent }
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, { useHash: true })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
