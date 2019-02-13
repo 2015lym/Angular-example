23b24fe869c8c86c0b5b62efff05f3c05737391e
2015lym <18622797149@163.com>
2019-02-13 16:03:38 +0800

    更新

---
+++ Section8/layout/src/app/cola/cola.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-cola',
+  templateUrl: './cola.component.html',
+  styleUrls: ['./cola.component.scss']
+})
+export class ColaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
