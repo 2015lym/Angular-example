b07089e0601791b4ccb807ebd005168a630adf93
2015lym <18622797149@163.com>
2019-02-23 17:35:27 +0800

    更新

--- Section9/nge2e/e2e/src/app.e2e-spec.ts
+++ Section9/nge2e/e2e/src/app.e2e-spec.ts
@@ -1,14 +1,57 @@
 import { AppPage } from './app.po';
+import { browser, by, element } from 'protractor';
 
 describe('workspace-project App', () => {
   let page: AppPage;
 
   beforeEach(() => {
     page = new AppPage();
+    browser.get('');
   });
 
-  it('should display welcome message', () => {
-    page.navigateTo();
-    expect(page.getTitleText()).toEqual('Welcome to nge2e!');
+  // 用户名
+  const username: any = element(by.id('e2e-username'));
+  // 密码
+  const password: any = element(by.id('e2e-password'));
+  // 登录按钮
+  const loginButton: any = element(by.id('e2e-login-button'));
+  // 保存密码
+  const savePassword: any = element(by.id('e2e-savePassword'));
+
+  it('网页标题为nge2e', () => {
+    browser.getTitle().then(res => {
+      expect(res).toBe('Nge2e');
+    });
+  });
+
+  it('不输入用户名和密码点击登录', () => {
+    loginButton.click();
+    browser.getCurrentUrl().then(res => {
+      console.log('xxx' + res);
+      expect(res).toBe('http://localhost:4200/');
+    });
+  });
+
+  it('不输入密码点击登录', () => {
+    browser.actions().mouseMove(username).click().sendKeys('zhangsan').perform();
+    browser.sleep(2000);
+    loginButton.click();
+    browser.sleep(2000);
+    browser.getCurrentUrl().then(res => {
+      expect(res).toBe('http://localhost:4200/');
+    });
+  });
+
+
+  it('输入用户名和密码点击登录', () => {
+    browser.actions().mouseMove(username).click().sendKeys('zhangsan').perform();
+    browser.sleep(2000);
+    browser.actions().mouseMove(password).click().sendKeys('123456').perform();
+    browser.sleep(2000);
+    loginButton.click();
+    browser.sleep(2000);
+    browser.getCurrentUrl().then(res => {
+      expect(res).toBe('http://localhost:4200/home');
+    });
   });
 });
--- Section9/nge2e/src/app/app.module.ts
+++ Section9/nge2e/src/app/app.module.ts
@@ -8,15 +8,21 @@ import { HttpClientModule } from '@angular/common/http';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 import { registerLocaleData } from '@angular/common';
 import en from '@angular/common/locales/en';
+import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home.component';
+import { AppRoutingModule } from './app-routing.module';
 
 registerLocaleData(en);
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
+    AppRoutingModule,
     NgZorroAntdModule,
     FormsModule,
     HttpClientModule,
