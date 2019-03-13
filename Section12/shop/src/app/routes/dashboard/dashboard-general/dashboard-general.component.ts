1151b4f5109a99b67662251e4b7aa300477fec13
2015lym <devilx2015@gmail.com>
2019-03-13 22:21:39 +0800

    更新

--- Section12/shop/src/app/routes/dashboard/dashboard-general/dashboard-general.component.ts
+++ Section12/shop/src/app/routes/dashboard/dashboard-general/dashboard-general.component.ts
@@ -7,19 +7,55 @@ import { _HttpClient } from '@delon/theme';
   styles: []
 })
 export class DashboardGeneralComponent implements OnInit {
+  // 头部数据
+  baseData = {};
+  // 列表数据
+  listData = [];
+  // 详细说明
+  mapOfExpandData = {};
+  // 排序名
+  sortName = null;
+  // 排序值
+  sortValue = null;
 
   constructor(private http: _HttpClient) { }
 
   ngOnInit() {
-    // this.http.get('/chart').subscribe((res: any) => {
-    //   res.offlineData.forEach((item: any, idx: number) => {
-    //     item.show = idx === 0;
-    //     item.chart = Object.assign([], res.offlineChartData);
-    //   });
-    //   this.data = res;
-    //   this.loading = false;
-    //   this.changeSaleType();
-    // });
+    this.getBaseData();
+    this.getListData();
+  }
+
+  // 获取顶部数据
+  getBaseData() {
+    this.http.get('http://localhost:3000/inventorLists').subscribe((res) => {
+      this.baseData = res;
+    });
+  }
+
+  // 获取列表数据
+  getListData() {
+    this.http.get('http://localhost:3000/commodity').subscribe((res: Array<Object>) => {
+      this.listData = res;
+    });
+  }
+
+  // 排序
+  sort(sort: { key: string, value: string }): void {
+    this.sortName = sort.key;
+    this.sortValue = sort.value;
+    this.search();
+  }
+
+  // 搜索
+  search(): void {
+    const data = [...this.listData];
+    if (this.sortName && this.sortValue) {
+      // tslint:disable-next-line:max-line-length
+      this.listData = data.sort((a, b) => (this.sortValue === 'ascend') ? (a[this.sortName] > b[this.sortName] ? 1 : -1) : (b[this.sortName] > a[this.sortName] ? 1 : -1));
+      console.log(this.listData);
+    } else {
+      this.listData = this.listData;
+    }
   }
 
 }
