00a8ace725c8013aa5048c2f16b994e3c6db84fd
2015lym <devilx2015@gmail.com>
2019-03-15 16:08:08 +0800

    增删改查

--- Section12/shop/src/app/routes/commodity/commodity-edit/commodity-edit.component.ts
+++ Section12/shop/src/app/routes/commodity/commodity-edit/commodity-edit.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { _HttpClient } from '@delon/theme';
+import { NzMessageService } from 'ng-zorro-antd';
 
 @Component({
   selector: 'app-commodity-edit',
@@ -6,10 +10,79 @@ import { Component, OnInit } from '@angular/core';
   styles: []
 })
 export class CommodityEditComponent implements OnInit {
+  validateForm: FormGroup;
+  id: '';
 
-  constructor() { }
+  constructor(private fb: FormBuilder,
+    private http: _HttpClient,
+    private router: Router,
+    private message: NzMessageService,
+    private activatedRoute: ActivatedRoute) {
+    this.validateForm = this.fb.group({
+      id: [null,],
+      pId: [null, [Validators.required, Validators.pattern(/^[0-9]*$/)]],
+      name: [null, [Validators.required]],
+      price: [null, [Validators.required, Validators.pattern(/^[0-9]*$/)]],
+      salesVolume: [null, [Validators.required, Validators.pattern(/^[0-9]*$/)]],
+      inventory: [null, [Validators.required]],
+      description: [null,]
+    });
+  }
 
-  ngOnInit() {
+  ngOnInit(): void {
+    if (this.activatedRoute.snapshot.queryParams['id']) {
+      this.id = this.activatedRoute.snapshot.queryParams['id'];
+      let url = 'http://localhost:3000/commodity?id=' + this.id;
+      this.http.get(url).subscribe((res: Array<Object>) => {
+        this.validateForm.setValue({
+          id: res[0]['id'],
+          pId: res[0]['pId'],
+          name: res[0]['name'],
+          price: res[0]['price'],
+          salesVolume: res[0]['salesVolume'],
+          inventory: res[0]['inventory'],
+          description: res[0]['description'] || ''
+        });
+      });
+    } else {
+      this.id = '';
+      this.validateForm.setValue({
+        id: '',
+        pId: '',
+        name: '',
+        price: '',
+        salesVolume: '',
+        inventory: '',
+        description: ''
+      });
+    }
+  }
+
+  submitForm(): void {
+    let params = {};
+    for (const i in this.validateForm.controls) {
+      this.validateForm.controls[i].markAsDirty();
+      this.validateForm.controls[i].updateValueAndValidity();
+      if (!(this.validateForm.controls[i].status == 'VALID') && this.validateForm.controls[i].status !== 'DISABLED') {
+        return;
+      }
+      if (this.validateForm.controls[i] && this.validateForm.controls[i].value) {
+        params[i] = this.validateForm.controls[i].value;
+      } else {
+        params[i] = '';
+      }
+    }
+    if (this.id == '') {
+      this.http.post('http://localhost:3000/commodity', params).subscribe((res) => {
+        this.message.success('添加成功');
+        this.router.navigate(['/commodity/search']);
+      });
+    } else {
+      this.http.put('http://localhost:3000/commodity/' + this.id, params).subscribe((res) => {
+        this.message.success('编辑成功');
+        this.router.navigate(['/commodity/search']);
+      });
+    }
   }
 
 }
--- Section12/shop/src/app/routes/commodity/commodity-search/commodity-search.component.ts
+++ Section12/shop/src/app/routes/commodity/commodity-search/commodity-search.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { _HttpClient } from '@delon/theme';
+import { NzModalRef, NzModalService, NzMessageService } from 'ng-zorro-antd';
 
 @Component({
   selector: 'app-commodity-search',
@@ -10,8 +11,12 @@ export class CommoditySearchComponent implements OnInit {
   // 列表数据
   listData = [];
   title = '';
+  // 对话框
+  confirmModal: NzModalRef;
 
-  constructor(private http: _HttpClient) { }
+  constructor(private http: _HttpClient,
+    private message: NzMessageService,
+    private modal: NzModalService) { }
 
   ngOnInit() {
     this.getListData();
@@ -39,5 +44,26 @@ export class CommoditySearchComponent implements OnInit {
   reset() {
     this.title = '';
     this.getListData();
+  }
+
+  // 编辑
+  edit() {
+
+  }
+
+  // 删除
+  delete(data) {
+    this.confirmModal = this.modal.confirm({
+      nzTitle: '提示',
+      nzContent: '确定要删除吗？',
+      nzOkText: '确定',
+      nzCancelText: '取消',
+      nzOnOk: () => {
+        this.http.delete('http://localhost:3000/commodity/' + data['id']).subscribe((res) => {
+          this.message.success('删除成功');
+          this.reset();
+        });
+      }
+    });
   }
 }
