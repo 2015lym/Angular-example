01f12821684f98e4f6edf8520a194eec5edaa85c
2015lym <devilx2015@gmail.com>
2019-03-17 16:51:45 +0800

    更新

--- Section12/shop/src/app/routes/person/person-detail/person-detail.component.ts
+++ Section12/shop/src/app/routes/person/person-detail/person-detail.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { _HttpClient } from '@delon/theme';
 
 @Component({
   selector: 'app-person-detail',
@@ -7,9 +8,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PersonDetailComponent implements OnInit {
 
-  constructor() { }
+  userInfo: Object = {};
+
+  constructor(private http: _HttpClient) { }
 
   ngOnInit() {
+    this.getUserInfo();
   }
 
+  getUserInfo() {
+    this.http.get('http://localhost:3000/user').subscribe((res) => {
+      this.userInfo = res;
+      console.log(this.userInfo);
+    });
+  }
 }
--- Section12/shop/src/app/routes/person/person-setting/person-setting.component.ts
+++ Section12/shop/src/app/routes/person/person-setting/person-setting.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { _HttpClient } from '@delon/theme';
+import { NzMessageService } from 'ng-zorro-antd';
 
 @Component({
   selector: 'app-person-setting',
@@ -6,10 +10,49 @@ import { Component, OnInit } from '@angular/core';
   styles: []
 })
 export class PersonSettingComponent implements OnInit {
+  validateForm: FormGroup;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(private fb: FormBuilder,
+    private http: _HttpClient,
+    private router: Router,
+    private message: NzMessageService,
+    private activatedRoute: ActivatedRoute) {
+    this.validateForm = this.fb.group({
+      avatar: [null, [Validators.required]],
+      username: [null, [Validators.required]],
+      status: [null, [Validators.required]],
+      sign: [null]
+    });
   }
 
+  ngOnInit() {
+    this.http.get('http://localhost:3000/user').subscribe((res) => {
+      this.validateForm.setValue({
+        avatar: res['avatar'],
+        username: res['username'],
+        status: res['status'],
+        sign: res['sign'] || ''
+      });
+    });
+  }
+
+  submitForm() {
+    let params = {};
+    for (const i in this.validateForm.controls) {
+      this.validateForm.controls[i].markAsDirty();
+      this.validateForm.controls[i].updateValueAndValidity();
+      if (!(this.validateForm.controls[i].status == 'VALID') && this.validateForm.controls[i].status !== 'DISABLED') {
+        return;
+      }
+      if (this.validateForm.controls[i] && this.validateForm.controls[i].value) {
+        params[i] = this.validateForm.controls[i].value;
+      } else {
+        params[i] = '';
+      }
+    }
+    this.http.put('http://localhost:3000/user/', params).subscribe((res) => {
+      this.message.success('修改成功');
+      this.router.navigate(['/person/detail']);
+    });
+  }
 }
