aaa74fe76cfe07cf410f9b1b82b51ba96516cd49
2015lym <devilx2015@gmail.com>
2019-03-20 13:45:01 +0800

    更新

--- Section5/user-info/src/app/app.component.spec.ts
+++ Section5/user-info/src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'date-picker'`, () => {
+  it(`should have as title 'user-info'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('date-picker');
+    expect(app.title).toEqual('user-info');
   });
 
   it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to date-picker!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to user-info!');
   });
 });
